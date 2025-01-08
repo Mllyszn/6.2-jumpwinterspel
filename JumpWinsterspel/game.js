be7e5281ce4072795e1b7fe31302d943a2c7007d
Mllyszn <melvinlieuw121107@gmail.com>
2025-01-08 14:37:58 +0100

    bg

--- JumpWinsterspel/game.js
+++ JumpWinsterspel/game.js
@@ -2,10 +2,23 @@
 //     var game = new Phaser.game();
 // }
 
-let gameScene = new phaser.scene('Game');
+let gameScene = new phaser.scene("Game");
 
-gameScene.init = function(){}
+gameScene.init = function () {};
 
-gameScene.preload = function(){}
+gameScene.preload = function () {
+    this.load.image('background', 'assets/img/sky.png');
+};
 
-gameScene.create = function(){}
+gameScene.create = function () {};
+
+let config = {
+    type: Phaser.AUTO,
+    width: 640,
+    height: 360,
+    scene: gameScene,
+    title: "winter jumpking",
+    pixelArt: false,
+};
+
+let game = new Phaser.Game(config);
