24e2d07f69eb0b10dec980ccef2d3f66e197b6a7
Mllyszn <melvinlieuw121107@gmail.com>
2025-01-09 16:03:34 +0100

    assets loading/ bug fixes

--- JumpWinsterspel/assets/js/main.js
+++ JumpWinsterspel/assets/js/main.js
@@ -10,7 +10,20 @@ const config = {
     type: Phaser.WEBGL,
     width: 500,
     height: 500,
-    canvas: gameCanvas
+    physics:{
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false,
+        },
+    },
+parent : "game-container",
+backgroundColor: "#028af8",
+scale:{
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+},
+  scene: [Boot, Preloader, MainMenu, Game, GameOver]
 }
 
 const game = new Phaser.Game(config)
--- JumpWinsterspel/game.js
+++ JumpWinsterspel/game.js
@@ -2,20 +2,25 @@
 //     var game = new Phaser.game();
 // }
 
-let gameScene = new phaser.scene("Game");
+let gameScene = new Phaser.Scene("Game");
 
 gameScene.init = function () {};
 
 gameScene.preload = function () {
     this.load.image('background', 'assets/img/sky.png');
+    this.load.image('platform', 'assets/img/platform.png');
+    this.load.image('character', 'assets/img/slime.png');
 };
 
-gameScene.create = function () {};
+gameScene.create = function () {
+
+    this.add.sprite(0,0, 'background').setOrigin(0,0);
+};
 
 let config = {
     type: Phaser.AUTO,
-    width: 640,
-    height: 360,
+    width: 650,
+    height: 400,
     scene: gameScene,
     title: "winter jumpking",
     pixelArt: false,
