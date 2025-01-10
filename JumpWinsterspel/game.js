85c09ec9a460a1eba51403d8aab945eeaf0bb682
Mllyszn <melvinlieuw121107@gmail.com>
2025-01-10 10:50:03 +0100

    platforms added / trying to add player

--- JumpWinsterspel/game.js
+++ JumpWinsterspel/game.js
@@ -7,20 +7,58 @@ let gameScene = new Phaser.Scene("Game");
 gameScene.init = function () {};
 
 gameScene.preload = function () {
-    this.load.image('background', 'assets/img/sky.png');
-    this.load.image('platform', 'assets/img/platform.png');
-    this.load.image('character', 'assets/img/slime.png');
+    this.load.image("background", "assets/img/sky.png");
+    this.load.image("ground", "assets/img/platform.png");
+    this.load.image("platform", "assets/img/air_grass-platform.png");
+    this.load.image("player", "assets/img/slime.png");
 };
+
+let player;
 
 gameScene.create = function () {
+    // background
+    this.add.sprite(0, 0, "background").setOrigin(0, 0);
+    // ground
+    this.add.sprite(0, 575, "ground").setOrigin(0, 0);
+    this.add.sprite(400, 575, "ground").setOrigin(0, 0);
+    // air platforms
+    this.add.sprite(50, 200, "platform").setOrigin(0, 0); //mid-left
+    this.add.sprite(400, 350, "platform").setOrigin(0, 0); //off-center
+    this.add.sprite(450, 50, "platform").setOrigin(0, 0); //top-right
+    // player
+    player = this.physics.add.sprite(100, 450, "player");
 
-    this.add.sprite(0,0, 'background').setOrigin(0,0);
+    player = this.physics.add.sprite(100, 450, "player");
+
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+
+    this.anims.create({
+        key: "left",
+        frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1,
+    });
+
+    this.anims.create({
+        key: "turn",
+        frames: [{ key: "dude", frame: 4 }],
+        frameRate: 20,
+    });
+
+    this.anims.create({
+        key: "right",
+        frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1,
+    });
+    // this.add.sprite(0, 0, "player").setOrigin(0, 0);
 };
 
-let config = {
+var config = {
     type: Phaser.AUTO,
-    width: 650,
-    height: 400,
+    width: 800,
+    height: 600,
     scene: gameScene,
     title: "winter jumpking",
     pixelArt: false,
