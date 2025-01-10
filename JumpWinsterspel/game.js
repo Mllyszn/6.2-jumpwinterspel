90f070c2a98803aef62c2f1ecb26c76c71bbe5bf
Mllyszn <melvinlieuw121107@gmail.com>
2025-01-10 11:26:31 +0100

    spritesheet jk added

--- JumpWinsterspel/game.js
+++ JumpWinsterspel/game.js
@@ -10,7 +10,11 @@ gameScene.preload = function () {
     this.load.image("background", "assets/img/sky.png");
     this.load.image("ground", "assets/img/platform.png");
     this.load.image("platform", "assets/img/air_grass-platform.png");
-    this.load.image("player", "assets/img/slime.png");
+    // this.load.image("player", "assets/img/slime.png");
+    this.load.spritesheet("player", "assets/img/jk_spritesheet.png", {
+        frameWidth: 32,
+        frameHeight: 48,
+    });
 };
 
 let player;
@@ -22,11 +26,10 @@ gameScene.create = function () {
     this.add.sprite(0, 575, "ground").setOrigin(0, 0);
     this.add.sprite(400, 575, "ground").setOrigin(0, 0);
     // air platforms
-    this.add.sprite(50, 200, "platform").setOrigin(0, 0); //mid-left
-    this.add.sprite(400, 350, "platform").setOrigin(0, 0); //off-center
-    this.add.sprite(450, 50, "platform").setOrigin(0, 0); //top-right
+    this.add.sprite(50, 175, "platform").setOrigin(0, 0); //mid-left
+    this.add.sprite(400, 330, "platform").setOrigin(0, 0); //off-center
+    this.add.sprite(450, 40, "platform").setOrigin(0, 0); //top-right
     // player
-    player = this.physics.add.sprite(100, 450, "player");
 
     player = this.physics.add.sprite(100, 450, "player");
 
@@ -35,24 +38,23 @@ gameScene.create = function () {
 
     this.anims.create({
         key: "left",
-        frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
+        frames: this.anims.generateFrameNumbers("player", { start: 0, end: 3 }),
         frameRate: 10,
         repeat: -1,
     });
 
     this.anims.create({
         key: "turn",
-        frames: [{ key: "dude", frame: 4 }],
+        frames: [{ key: "player", frame: 4 }],
         frameRate: 20,
     });
 
     this.anims.create({
         key: "right",
-        frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
+        frames: this.anims.generateFrameNumbers("player", { start: 5, end: 8 }),
         frameRate: 10,
         repeat: -1,
     });
-    // this.add.sprite(0, 0, "player").setOrigin(0, 0);
 };
 
 var config = {
