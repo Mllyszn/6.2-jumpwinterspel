a9ba1254b7f09b5d98819f32c66d7ad3cd7628ff
Mllyszn <melvinlieuw121107@gmail.com>
2025-01-10 14:10:20 +0100

    trying to add physics/movement to player

--- JumpWinsterspel/game.js
+++ JumpWinsterspel/game.js
@@ -10,14 +10,18 @@ gameScene.preload = function () {
     this.load.image("background", "assets/img/sky.png");
     this.load.image("ground", "assets/img/platform.png");
     this.load.image("platform", "assets/img/air_grass-platform.png");
-    // this.load.image("player", "assets/img/slime.png");
-    this.load.spritesheet("player", "assets/img/jk_spritesheet.png", {
+    this.load.image("player", "assets/img/slime.png", {
         frameWidth: 32,
         frameHeight: 48,
     });
+    // this.load.spritesheet("player", "assets/img/jk_spritesheet.png", {
+    //     frameWidth: 32,
+    //     frameHeight: 48,
+    // });
 };
 
 let player;
+var platforms;
 
 gameScene.create = function () {
     // background
@@ -31,30 +35,35 @@ gameScene.create = function () {
     this.add.sprite(450, 40, "platform").setOrigin(0, 0); //top-right
     // player
 
-    player = this.physics.add.sprite(100, 450, "player");
+    this.add.sprite(450, 40, "player").setOrigin(0, 0);
+    new Phaser.Physics.Arcade.Sprite(this, 100, 100, "player").setOrigin();
 
-    player.setBounce(0.2);
-    player.setCollideWorldBounds(true);
+    player.create = function () {
+        player = this.physics.add.sprite(100, 450, "player");
+        player.setScale(0.5);
+        player.setBounce(0.2);
+        player.setCollideWorldBounds(true);
+    };
 
-    this.anims.create({
-        key: "left",
-        frames: this.anims.generateFrameNumbers("player", { start: 0, end: 3 }),
-        frameRate: 10,
-        repeat: -1,
-    });
+    // this.anims.create({
+    //     key: "left",
+    //     frames: this.anims.generateFrameNumbers("player", { start: 0, end: 3 }),
+    //     frameRate: 10,
+    //     repeat: -1,
+    // });
 
-    this.anims.create({
-        key: "turn",
-        frames: [{ key: "player", frame: 4 }],
-        frameRate: 20,
-    });
+    // this.anims.create({
+    //     key: "turn",
+    //     frames: [{ key: "player", frame: 4 }],
+    //     frameRate: 20,
+    // });
 
-    this.anims.create({
-        key: "right",
-        frames: this.anims.generateFrameNumbers("player", { start: 5, end: 8 }),
-        frameRate: 10,
-        repeat: -1,
-    });
+    // this.anims.create({
+    //     key: "right",
+    //     frames: this.anims.generateFrameNumbers("player", { start: 5, end: 8 }),
+    //     frameRate: 10,
+    //     repeat: -1,
+    // });
 };
 
 var config = {
