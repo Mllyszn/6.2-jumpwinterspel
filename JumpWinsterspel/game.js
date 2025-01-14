bd0ada0f8dd3caa474486285add4988506f42d48
Mllyszn <melvinlieuw121107@gmail.com>
2025-01-14 09:13:43 +0100

    Update game.js

--- JumpWinsterspel/game.js
+++ JumpWinsterspel/game.js
@@ -1,61 +1,62 @@
-// Window.onload= function(){
-//     var game = new Phaser.game();
-// }
+// Create a new Phaser game instance
+var config = {
+    type: Phaser.CANVAS,
+    width: 800,
+    height: 600,
+    backgroundColor: '#f3cca3',
+    physics: {
+        default: "arcade",
+        arcade: {
+            gravity: { y: 0 }, // No gravity for a platformer
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
 
-// const { Physics } = require("phaser");
+var game = new Phaser.Game(config);
 
+let player;
+let platforms;
 
-var game = new Phaser.Game(800,600,Phaser.CANVAS,'gameDiv');
-// const game = new Phaser.Game(config);
-
-// let gameScene = new Phaser.Scene("Game");
-
-gameScene.init = function () {};
-
-gameScene.preload = function () {
+function preload() {
     this.load.image("background", "assets/img/sky.png");
     this.load.image("ground", "assets/img/platform.png");
     this.load.image("platform", "assets/img/air_grass-platform.png");
-    this.load.image("player", "assets/img/slime.png", {
-        frameWidth: 32,
-        frameHeight: 48,
-    });
     this.load.spritesheet("player", "assets/img/jk_spritesheet.png", {
         frameWidth: 32,
         frameHeight: 48,
     });
-};
+}
 
-let player;
-var platforms;
-
-// physics!!!
-
-const { Physics } = require("phaser");
-this.physics.add.collider(player, platform);
-
-gameScene.create = function () {
-    // background
+function create() {
+    // Background
     this.add.sprite(0, 0, "background").setOrigin(0, 0);
-    // ground
-    this.add.sprite(0, 575, "ground").setOrigin(0, 0);
-    this.add.sprite(400, 575, "ground").setOrigin(0, 0);
-    // air platforms
-    this.add.sprite(50, 175, "platform").setOrigin(0, 0); //mid-left
-    this.add.sprite(400, 330, "platform").setOrigin(0, 0); //off-center
-    this.add.sprite(450, 40, "platform").setOrigin(0, 0); //top-right
-    // player
 
-    this.add.sprite(450, 40, "player").setOrigin(0, 0);
-    new Phaser.Physics.Arcade.Sprite(this, 100, 100, "player").setOrigin();
+    // Create platforms group
+    platforms = this.physics.add.staticGroup();
 
-    player.create = function () {
-        player = this.physics.add.sprite(100, 450, "player");
-        player.setScale(0.5);
-        player.setBounce(0.2);
-        player.setCollideWorldBounds(true);
-    };
+    // Ground
+    platforms.create(400, 575, "ground").setOrigin(0.5, 0).refreshBody();
+    platforms.create(600, 400, "ground").setOrigin(0.5, 0).refreshBody();
+    platforms.create(50, 175, "platform").setOrigin(0.5, 0).refreshBody(); // mid-left
+    platforms.create(400, 330, "platform").setOrigin(0.5, 0).refreshBody(); // off-center
+    platforms.create(450, 40, "platform").setOrigin(0.5, 0).refreshBody(); // top-right
 
+    // Player
+    player = this.physics.add.sprite(100, 450, "player");
+    player.setScale(0.5);
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+
+    // Collide player with platforms
+    this.physics.add.collider(player, platforms);
+
+    // Animations
     this.anims.create({
         key: "left",
         frames: this.anims.generateFrameNumbers("player", { start: 0, end: 3 }),
@@ -75,32 +76,23 @@ gameScene.create = function () {
         frameRate: 10,
         repeat: -1,
     });
-};
+}
 
-// var config = {
-//     type: Phaser.AUTO,
-//     width: 800,
-//     height: 600,
-//     scene: gameScene,
-//     title: "winter jumpking",
-//     pixelArt: false,
-// };
-
-let config = {
-    type: Phaser.AUTO,
-    width: 1000,
-    height: 615,
-    physics: {
-      default: "arcade",
-      arcade: {
-        debug: false
-      }
-    },
-    scene: {
-      preload: preload,
-      create: create,
-      update: update
+function update() {
+    // Handle player input for movement
+    if (this.input.keyboard.isDown(Phaser.Input.Keyboard.KeyCodes.LEFT)) {
+        player.setVelocityX(-160);
+        player.anims.play("left", true);
+    } else if (this.input.keyboard.isDown(Phaser.Input.Keyboard.KeyCodes.RIGHT)) {
+        player.setVelocityX(160);
+        player.anims.play("right", true);
+    } else {
+        player.setVelocityX(0);
+        player.anims.play("turn");
     }
-  };
 
-// let game = new Phaser.Game(config);
+    // Jumping
+    if (this.input.keyboard.isDown(Phaser.Input.Keyboard.KeyCodes.UP) && player.body.touching.down) {
+        player.setVelocityY(-330);
+    }
+}
