4a4aadf7e706f2a5c3d9859b3311d7a448f2aa04
Mllyszn <melvinlieuw121107@gmail.com>
2025-01-15 14:51:41 +0100

    movemnet fix

--- JumpWinsterspel/game.js
+++ JumpWinsterspel/game.js
@@ -3,18 +3,17 @@ var config = {
     type: Phaser.CANVAS,
     width: 800,
     height: 600,
-    // backgroundColor: '#f3cca3', //background color
     physics: {
         default: "arcade",
         arcade: {
-            gravity: { y: 0 },
+            gravity: { y: 350 },
             debug: false,
         },
     },
     scene: {
         preload: preload,
         create: create,
-        update: update, //comment = game werkend - uncomment = game niet werkend
+        update: update, 
     },
 };
 
@@ -22,12 +21,18 @@ var game = new Phaser.Game(config);
 
 let player;
 let platforms;
+let isMoving;
+let cursors;
 
 function preload() {
     this.load.image("background", "assets/img/sky.png");
     this.load.image("ground", "assets/img/platform.png");
     this.load.image("platform", "assets/img/air_grass-platform.png");
-    this.load.atlas('player', 'assets/img/player.png', 'assets/json/player.json');
+    this.load.atlas(
+        "player",
+        "assets/img/player.png",
+        "assets/json/player.json"
+    );
 
     // this.load.spritesheet("player", "assets/img/jk_spritesheet_melvin.png", {
     //     frameWidth: 32,
@@ -59,75 +64,99 @@ function create() {
 
     // Animations
     this.anims.create({
-        key: 'idle',
-        frames: this.anims.generateFrameNames('player', { prefix: 'idle/frame', start: 0, end: 5, zeroPad: 4 }),
+        key: "idle",
+        frames: this.anims.generateFrameNames("player", {
+            prefix: "idle/frame",
+            start: 0,
+            end: 5,
+            zeroPad: 4,
+        }),
         frameRate: 8,
-        repeat: -1
-    });
-
-    this.anims.create({
-        key: "left",
-        frames: this.anims.generateFrameNumbers("player", { start: 0, end: 4 }),
-        frameRate: 10,
         repeat: -1,
     });
 
-    this.anims.create({
-        key: "turn",
-        frames: [{ key: "player", frame: 4 }],
-        frameRate: 20,
-    });
+    cursors = this.input.keyboard.createCursorKeys();
+    isMoving = false;
 
-    this.anims.create({
-        key: "right",
-        frames: this.anims.generateFrameNumbers("player", { start: 5, end: 8 }),
-        frameRate: 10,
-        repeat: -1,
-    });
+    // this.anims.create({
+    //     key: "left",
+    //     frames: this.anims.generateFrameNumbers("player", { start: 0, end: 4 }),
+    //     frameRate: 10,
+    //     repeat: -1,
+    // });
 
-    // var lancelot = this.add.sprite(500, 536)
+    // this.anims.create({
+    //     key: "turn",
+    //     frames: [{ key: "player", frame: 4 }],
+    //     frameRate: 20,
+    // });
+
+    // this.anims.create({
+    //     key: "right",
+    //     frames: this.anims.generateFrameNumbers("player", { start: 5, end: 8 }),
+    //     frameRate: 10,
+    //     repeat: -1,
+    // });
+
+    // var lancelot = this.add.sprite(500, 536);
 
     // lancelot.setOrigin(0.5, 1);
-    //     lancelot.setScale(8);
-    //     lancelot.play('idle');
+    // lancelot.setScale(8);
+    // lancelot.play("idle");
+    // lancelot.on(Phaser.Animations.Events.ANIMATION_START, function (anim) {
+    //     text.setText("Playing " + anim.key);
+    // });
 }
 
-
 function update() {
-    this.input.keyboard.on("keydown", function (event) {
-        if (event.key=='ArrowLeft') {
+
+    if (cursors.left.isDown)
+        {
             player.setVelocityX(-160);
-            player.anims.play("left", true);
-        } else if (
-            event.key=='ArrowRight'
-        ) {
+
+            player.anims.play('left', true);
+        }
+        else if (cursors.right.isDown)
+        {
             player.setVelocityX(160);
-            player.anims.play("right", true);
-        } else {
+
+            player.anims.play('right', true);
+        }
+        else
+        {
             player.setVelocityX(0);
-            player.anims.play("turn");
+
+            player.anims.play('turn');
         }
 
-        // Jumping/springen
-        if (
-            event.key=='ArrowUp'
-        ) {
+        if (cursors.up.isDown && player.body.touching.down)
+        {
             player.setVelocityY(-330);
         }
-    });
-    // if (this.input.keyboard.KeyDow(Phaser.Input.Keyboard.KeyCodes.LEFT)) {
-    //     player.setVelocityX(-160);
-    //     player.anims.play("left", true);
-    // } else if (this.input.keyboard.KeyDown(Phaser.Input.Keyboard.KeyCodes.RIGHT)) {
-    //     player.setVelocityX(160);
-    //     player.anims.play("right", true);
-    // } else {
+    // this.input.keyboard.on("keydown", function (event) {
+    //     if (event.key == "ArrowLeft") {
+    //         isMoving;
+    //         player.setVelocityX(-160);
+    //        // player.anims.play("left", true);
+    //     } else if (event.key == "ArrowRight") {
+    //         isMoving;
+    //         player.setVelocityX(160);
+    //        // player.anims.play("right", true);
+    //     } else if(player.body.deltaY() > 0 && player.body.onFloor()){
+    //         isMoving = false;
+    //         player.setVelocityX(0);
+    //         //player.anims.play("turn", true);
+    //     }
+
+    //     // Jumping/springen
+    //     if (event.key == "ArrowUp") {
+    //         player.setVelocityY(-350);
+    //     }
+    // }); 
+    
+    // if(isMoving == false && player.body.deltaY() > 0 && player.body.onFloor()){
     //     player.setVelocityX(0);
-    //     player.anims.play("turn");
+    //     //player.anims.play("turn", true);
     // }
 
-    // // Jumping/springen
-    // if (this.input.keyboard.isDown(Phaser.Input.Keyboard.KeyCodes.UP) && player.body.touching.down) {
-    //     player.setVelocityY(-330);
-    // }
 }
